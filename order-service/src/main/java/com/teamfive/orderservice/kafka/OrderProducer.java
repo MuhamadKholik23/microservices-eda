1cc9df00476d9b3365413f8b4942d0243f65d113
muhamadkholik23 <muhamadkholikduatilu@gmail.com>
2023-05-28 15:00:15 +0700

    add base domain

---
+++ order-service/src/main/java/com/teamfive/orderservice/kafka/OrderProducer.java
@@ -0,0 +1,11 @@
+package com.teamfive.orderservice.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.protocol.Message;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class OrderProducer {
+  private NewTopic topic;
+  
+
+}
